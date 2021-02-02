ccbe33612b7054a114f71083955c037ce31ffd4d
sosoSO <68050435+SoJeonghu@users.noreply.github.com>
2021-02-02 21:10:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-const storage=require('@google-cloud/storage');
+const storage = require('@google-cloud/storage')();
 const sharp = require('sharp');
 
-exports.resizeAndUpload= (data ,context)=>{
-  const {bucket , name} = data;
-  const ext = name.split('.')[key.split('.').length-1];
-  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
-  console.log('name',name,'ext',ext);
+exports.resizeAndUpload = (data, context) => {
+  const { bucket, name } = data;
+  const ext = name.split('.')[name.split('.').length - 1];
+  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext; // sharp에서는 jpg 대신 jpeg사용합니다
+  console.log('name', name, 'ext', ext);
 
   const file = storage.bucket(bucket).file(name);
   const readStream = file.createReadStream();
@@ -15,15 +15,14 @@ exports.resizeAndUpload= (data ,context)=>{
   const writeStream = newFile.createWriteStream();
 
   sharp(readStream)
-      .resize(200,200,{fit:'inside'})
+      .resize(200, 200, { fit: 'inside' })
       .toFormat(requiredFormat)
       .pipe(writeStream);
-  return new Promise((resolve ,reject )=>{
-    writeStream.on('finish',()=>{
+  return new Promise((resolve, reject) => {
+    writeStream.on('finish', () => {
       resolve(`thumb/${name}`);
     });
-    writeStream.on('error',reject);
-  })
-
-}
+    writeStream.on('error', reject);
+  });
+};
 
